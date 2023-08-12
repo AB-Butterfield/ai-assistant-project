b0452da72f72d2a0be87e11863128c5316cb2c2b
Alec B <alec.butterfield1@gmail.com>
2023-08-12 12:36:24 -0700

    installed pyautogui

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pyttsx3
 import webbrowser
 import wikipedia
 import wolframalpha
+import subprocess
 
 # Speech engine initialisation
 engine = pyttsx3.init()
@@ -12,6 +13,10 @@ engine.setProperty('voice', voices[1].id) # 0 for male, 1 for female
 activationWord = 'computer' #Kiko?
 # sr.adjust_for_ambient_noise(source, duration=5)
 
+# Configure browser
+chrome_path = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
+
 def speak(text, rate = 120):
     engine.setProperty('rate', rate)
     engine.say(text)
@@ -19,6 +24,7 @@ def speak(text, rate = 120):
 
 def parseCommand():
     listener = sr.Recognizer()
+    print('Adjusting for ambient noise...')
     with sr.Microphone() as source:
         listener.adjust_for_ambient_noise(source) 
     print('Listening for your command')
@@ -33,7 +39,7 @@ def parseCommand():
         print(f'The input speech was: {query}')
     except Exception as exception:
         print('I didnt quite catch that')
-        speak('Sah ry I didnt catch that')
+        # speak('Sah ry I didnt catch that')
         print(exception)
         return 'None'
     
@@ -51,8 +57,8 @@ if __name__ == '__main__':
         if query[0] == activationWord:
             query.pop(0)
             print(query)
+
             # List commands
-            
             if query[0] == 'say':
                 if query[1] == 'your':
                     if query[2] == 'name':
@@ -64,6 +70,43 @@ if __name__ == '__main__':
                     query.pop(0) #remove say
                     speech = ' '.join(query)
                     speak(speech)
+
+            # Navigation
+            if query[0] == 'go' and query[1] == 'to':
+                speak('Opening...')
+                query = ' '.join(query[2:])
+                webbrowser.get('chrome').open_new(query)
+
+            # Helpful Tasks
+            if query[0] == 'please':
+
+                # Take a note on Word
+                if query[1] == 'take' and query[2] == 'a' and query[3] == 'note':
+                    subprocess.Popen("C:\WINDOWS\system32\notepad.exe")
+
+
+            # Apps to open
+            if query[0] == 'open':
+                query = ' '.join(query[1:])
+                print(query)
+                # League of Legends 
+                if query == 'league of legends':
+                    speak('Opening league')
+                    subprocess.Popen("D:\Riot Games\Riot Client\RiotClientServices.exe")
+
+                # Microsoft Word
+
+                # Opera GX
+
+                # Krita
+
+
+
+            #Exiting
+            if query[0] == 'exit':
+                speak('Signing off')
+                break
+
 
 
 
